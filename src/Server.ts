dd45f5edb5f8297252f5e0d0d34d227ed90de477
akhileshkumbhkar <akhilesh.kumbhkar@successive.tec>
2020-10-29 13:33:17 +0530

    Feat : HTTP Status code and handlers

--- src/Server.ts
+++ src/Server.ts
@@ -1,28 +1,36 @@
-import * as express from 'express' ;
+import * as express from 'express';
+import * as bodyparser from 'body-parser';
+import  { notFoundRoute , errorHandler } from './libs/routes' ;
 class Server {
-app ;
- constructor (private config) {
- this.app = express() ;
-}
-bootstrap() {
-this.setupRoutes();
-return this ;
-}
-setupRoutes() {
-const { app } = this;
-app.use('./health-check' , (req, res, next) => {
-res.send('i am ok') ;
- }) ;
- return this ;
-}
-run() {
-const { app , config: { PORT} } = this ;
-app.listen(PORT, (err) => {
-if (err) {
- console.log(err) ;
+    app;
+    constructor(private config) {
+        this.app = express();
  }
- console.log(`app is running on port ${PORT}` ) ;
-  }) ;
-}
-}
-export default Server ;
+public initBodyParser() {
+        this.app.use(bodyparser.json());
+    }
+    bootstrap() {
+        this.initBodyParser();
+        this.setupRoutes();
+        return this;
+     }
+public setupRoutes() {
+this.app.use('/health-check', (req, res, next) => {
+res.send('I am Ok');
+next();
+  });
+ this.app.use(notFoundRoute);
+ this.app.use(errorHandler);
+ return this;
+  }
+run () {
+ const { app , config : {PORT }} = this;
+ app.listen(PORT, (err) => {
+ if (err) {
+console.log(err);
+ }
+ console.log(`App is running on port ${PORT}`);
+ });
+ }
+ }
+export default Server;
